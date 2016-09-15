cc128406bb7864d9e7ecfe7b65b3b5c508e0819d
Rusty Holleman <rustychris@gmail.com>
2016-09-14 21:42:55 -0700

    Significant progress, can make two cells now.

--- stompy/grid/front.py
+++ stompy/grid/front.py
@@ -24,6 +24,9 @@ class Curve(object):
     by a floating point value (including modulo arithmetic).
     By default, indexes by distance along each segment.
     """
+    class CurveException(Exception):
+        pass
+    
     def __init__(self,points,closed=True):
         self.points=np.asarray(points)
         self.closed=closed
@@ -41,7 +44,8 @@ class Curve(object):
             idxs=np.searchsorted(self.distances,f,side='right') - 1
             
             alphas = (f - self.distances[idxs]) / (self.distances[idxs+1]-self.distances[idxs])
-            alphas = alphas[:,None]
+            if not np.isscalar(alphas):
+                alphas = alphas[:,None]
             return (1-alphas)*self.points[idxs] + alphas*self.points[idxs+1]
         else:
             assert False
@@ -78,8 +82,69 @@ class Curve(object):
             return new_points,sources
         else:
             return new_points
-        
 
+    def distance_away(self,anchor_f,signed_distance,rtol=0.05):
+        """  Find a point on the curve signed_distance away from the
+        point corresponding to anchor_f, within the given relative tolerance.
+        returns new_f,new_x.
+
+        If a point could not be found within the requested tolerance, raises
+        a self.CurveException.
+
+        Starting implementation is weak - it ignores any knowledge of the piecewise
+        linear geometry.  Will need to be amended to take that into account, since 
+        in its current state it will succumb to local minima/maxima.
+        """
+        anchor_x = self(anchor_f)
+        offset=signed_distance
+        direc=np.sign(signed_distance)
+        target_d=np.abs(signed_distance)
+
+        last_offset=0.0
+        last_d=0.0
+
+        # first loop to bracket the distance:
+        for step in range(10): # ad-hoc limit to avoid bad juju.
+            new_x=self(anchor_f + offset)
+            d=utils.dist(anchor_x-new_x)
+            rel_err=(d - target_d)/target_d
+            if -rtol < rel_err < rtol:
+                return anchor_f + offset,new_x
+            if rel_err<0:
+                if d<last_d:
+                    # this could easily be a local minimum - as this becomes important,
+                    # then it would be better to include some bounds checking, and only
+                    # fail when we can prove that no solution exists.
+                    raise self.CurveException("Distance got smaller - need to be smarter")
+                last_offset=offset
+                last_d=d
+                offset*=1.5
+                continue
+            else:
+                break # can binary search
+        # binary search
+        low_offset=last_offset
+        high_offset=offset
+        for step in range(10):
+            mid_offset = 0.5*(low_offset + high_offset)
+            mid_x = self(anchor_f + mid_offset)
+            mid_d = utils.dist(anchor_x - mid_x)
+            rel_err=(mid_d - target_d)/target_d
+            if -rtol<rel_err<rtol:
+                return anchor_f+mid_offset,mid_x
+            elif mid_d < target_d:
+                low_offset=mid_offset
+            else:
+                high_offset=mid_offset
+        else:
+            raise self.CurveException("Binary search failed")
+
+    def is_forward(self,fa,fb,fc):
+        d=self.total_distance()
+        return ((fb-fa) % d) < ((fc-fa)%d)
+    def is_reverse(self,fa,fb,fc):
+        return self.is_forward(fc,fb,fa)
+    
     def plot(self,ax=None):
         ax=ax or plt.gca()
         return ax.plot(self.points[:,0],self.points[:,1])[0]
@@ -111,17 +176,29 @@ class TriangleSite(object):
     When adding triangles, the heuristic is to choose
     tight locations.
     """
-    def __init__(self,grid,nodes):
-        self.grid=grid
+    def __init__(self,af,nodes):
+        self.af=af
+        self.grid=af.grid
         assert len(nodes)==3
         self.abc = nodes
     def metric(self):
-        return self.internal_angle()
+        return self.internal_angle
     def points(self):
         return self.grid.nodes['x'][ self.abc ]
+    
+    @property
     def internal_angle(self):
         A,B,C = self.points() 
         return internal_angle(A,B,C)
+    @property
+    def edge_length(self):
+        return utils.dist( np.diff(self.points(),axis=0) ).mean()
+    
+    @property
+    def local_length(self):
+        scale = self.af.scale
+        return scale( self.points().mean(axis=0) )
+
     def plot(self,ax=None):
         ax=ax or plt.gca()
         points=self.grid.nodes['x'][self.abc]
@@ -210,7 +287,7 @@ class AdvancingFront(object):
             c=he_nxt.node_fwd()
             assert b==bb
 
-            sites.append( TriangleSite(self.grid,nodes=[a,b,c]) )
+            sites.append( TriangleSite(self,nodes=[a,b,c]) )
         if len(sites):
             scores=[ site.metric()
                      for site in sites ]
@@ -218,3 +295,102 @@ class AdvancingFront(object):
             return sites[best]
         else:
             return None
+        
+    def free_span(self,he,max_span,direction):
+        span=0.0
+        if direction==1:
+            trav=he.node_fwd()
+            last=anchor=he.node_rev()
+        else:
+            trav=he.node_rev()
+            last=anchor=he.node_fwd()
+
+        def pred(n):
+            return ( (self.grid.nodes['fixed'][n]== self.SLIDE) and
+                     len(self.grid.node_to_edges(n))<=2 )
+
+        while pred(trav) and (trav != anchor) and (span<max_span):
+            span += utils.dist( self.grid.nodes['x'][last] -
+                                 self.grid.nodes['x'][trav] )
+            if direction==1:
+                he=he.fwd()
+                last,trav = trav,he.node_fwd()
+            elif direction==-1:
+                he=he.rev()
+                last,trav = trav,he.node_rev()
+            else:
+                assert False
+        return span
+    
+    max_span_factor=4     
+    def resample(self,n,anchor,scale,direction):
+        self.log.debug("resample %d to be  %g away from %d in the %s direction"%(n,scale,anchor,
+                                                                                 direction) )
+        if direction==1: # anchor to n is t
+            he=self.grid.nodes_to_halfedge(anchor,n)
+        elif direction==-1:
+            he=self.grid.nodes_to_halfedge(n,anchor)
+        else:
+            assert False
+
+        span_length = self.free_span(he,self.max_span_factor*scale,direction)
+        self.log.debug("free span from the anchor is %g"%span_length)
+
+        if span_length < self.max_span_factor*scale:
+            n_segments = max(1,round(span_length / scale))
+            target_span = span_length / n_segments
+            if n_segments==1:
+                self.log.debug("Only one space for 1 segment")
+                return
+        else:
+            target_span=scale
+
+        # first, find a point on the original ring which satisfies the target_span
+        oring=self.grid.nodes['oring'][anchor]
+        curve = self.curves[oring]
+        anchor_f = self.grid.nodes['ring_f'][anchor]
+        try:
+            new_f,new_x = curve.distance_away(anchor_f,direction*target_span)
+        except curve.CurveException as exc:
+            raise
+
+        # check to see if there are other nodes in the way, and remove them.
+        nodes_to_delete=[]
+        trav=he
+        while True:
+            if direction==1:
+                trav=trav.fwd()
+            else:
+                trav=trav.rev()
+            # we have anchor, n, and
+            if trav==he:
+                self.log.error("Made it all the way around!")
+                raise Exception("This is probably bad")
+
+            if direction==1:
+                n_trav=trav.node_fwd()
+                f_trav=self.grid.nodes['ring_f'][n_trav]
+                if curve.is_forward( anchor_f, new_f, f_trav ):
+                    break
+            else:
+                n_trav=trav.node_rev()
+                f_trav=self.grid.nodes['ring_f'][n_trav]
+                if curve.is_reverse( anchor_f, new_f, f_trav ):
+                    break
+
+            nodes_to_delete.append(n_trav)
+
+        for d in nodes_to_delete:
+            self.grid.merge_edges(node=d)
+
+        self.grid.modify_node(n,x=new_x,ring_f=new_f)
+
+    def resample_neighbors(self,site):
+        a,b,c = site.abc
+        local_length = self.scale( site.points().mean(axis=0) )
+
+        for n,direction in [ (a,-1),
+                             (c,1) ]:
+            if ( (self.grid.nodes['fixed'][n] == self.SLIDE) and
+                 len(self.grid.node_to_edges(n))<=2 ):
+                self.resample(n=n,anchor=b,scale=local_length,direction=direction)
--- stompy/grid/test_front.py
+++ stompy/grid/test_front.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import field
+from scipy import optimize as opt
 import unstructured_grid
 reload(unstructured_grid)
 import front
@@ -30,6 +31,43 @@ def test_curve_eval():
         crvX=crv(f)
         plt.plot(crvX[:,0],crvX[:,1],'ro')
 
+def test_distance_away():
+    crv=hex_curve()
+
+    if plt:
+        plt.clf()
+        crv.plot()
+        plt.axis('equal')
+        
+    rtol=0.05
+
+    for f00,tgt,style in [ (0,10,'g-'),
+                           (3.4,20,'r-'),
+                           (3.4,-20,'r--') ]:
+        for f0 in np.linspace(f00,crv.distances[-1],20):
+            x0=crv(f0)
+            f,x =crv.distance_away(f0,tgt,rtol=rtol)
+            d=utils.dist(x-x0)
+            assert np.abs( (d-np.abs(tgt))/tgt) <= rtol
+            if plt:
+                plt.plot( [x0[0],x[0]],
+                          [x0[1],x[1]],style)
+
+    try:
+        f,x=crv.distance_away(0.0,50,rtol=0.05)
+        raise Exception("That was supposed to fail!")
+    except crv.CurveException:
+        #print "Okay"
+        pass
+
+def test_is_forward():
+    crv=hex_curve()
+    assert crv.is_forward(5,6,50)
+    assert crv.is_reverse(5,-5,10)
+
+
+
+#-# 
 def test_curve_upsample():
     boundary=hex_curve()
     scale=field.ConstantField(3)
@@ -112,12 +150,81 @@ def test_halfedge_traverse():
     assert he.rev().fwd() == he
     #-# 
 
-test_halfedge_traverse()
+def test_merge_edges():
+    af=test_basic_setup()
 
-af=test_basic_setup()
+    new_j=af.grid.merge_edges(node=0)
+    
+    he0=he=af.grid.halfedge(new_j,0)
+    c0_left = af.grid.edges['cells'][he.j,he.orient]
+    c0_right = af.grid.edges['cells'][he.j,1-he.orient]
 
-site=af.choose_site()
-site.plot()
+    while True:
+        he=he.fwd()
+        c_left = af.grid.edges['cells'][he.j,he.orient]
+        c_right = af.grid.edges['cells'][he.j,1-he.orient]
+        assert c_left==c0_left
+        assert c_right==c0_right
+        
+        if he==he0:
+            break
+
+
+    if plt:
+        plt.clf()
+        af.grid.plot_edges()
+
+        coll=af.grid.plot_halfedges(values=af.grid.edges['cells'])
+        coll.set_lw(0)
+        coll.set_cmap('winter')
+
+# when resample nodes on a sliding boundary, want to calculate the available
+# span, and if it's small, start distributing the nodes evenly.
+# where small is defined by local_scale * max_span_factor
+
+
+def test_resample():
+    af=test_basic_setup()
+    a=0
+    b=af.grid.node_to_nodes(a)[0]
+    he=af.grid.nodes_to_halfedge(a,b)
+    anchor=he.node_rev()
+    n=he.node_fwd()
+    n2=he.rev().node_rev()
+    af.resample(n=n,anchor=anchor,scale=25,direction=1)
+    af.resample(n=n2,anchor=anchor,scale=25,direction=-1)
+
+    if plt:
+        plt.clf()
+        af.grid.plot_edges()
+
+        coll=af.grid.plot_halfedges(values=af.grid.edges['cells'])
+        coll.set_lw(0)
+        coll.set_cmap('winter')
+    
+    
+#-#     
+
+
+
+def test_resample_neighbors():
+    af=test_basic_setup()
+    
+    if plt:
+        plt.clf()
+        af.grid.plot_nodes(color='r')
+    
+    site=af.choose_site()
+            
+    af.resample_neighbors(site)
+
+    if plt:
+        af.grid.plot_edges()
+
+        af.grid.plot_nodes(color='g')
+        plt.axis( [34.91, 42.182, 7.300, 12.97] )
+        
+test_resample_neighbors()
 
 ## 
 
@@ -128,74 +235,262 @@ site.plot()
 # but the idea of having them spaced at the local scale when possible
 # is general enough to do it preemptively.
 
-def free_span(he,max_span,direction):
-    span=0.0
-    if direction==1:
-        trav=he.node_fwd()
-        last=anchor=he.node_rev()
-    else:
-        trav=he.node_rev()
-        last=anchor=he.node_fwd()
+# strategies:
+#  try this as a separate class for each strategy, but they are all singletons
 
-    def pred(n):
-        return ( (self.grid.nodes['fixed'][n]== self.SLIDE) and
-                 len(self.grid.node_to_edges(n))<=2 )
 
-    while pred(trav) and (trav != anchor) and (span<max_span):
-        span += utils.dist( self.grid.nodes['x'][last] -
-                             self.grid.nodes['x'][trav] )
-        if direction==1:
-            he=he.fwd()
-            last,trav = trav,he.node_fwd()
-        elif direction==-1:
-            he=he.rev()
-            last,trav = trav,he.node_rev()
-        else:
-            assert False
-    return span
+class Strategy(object):
+    def metric(self,site,scale_factor):
+        assert False
+    def execute(self,site):
+        """
+        Apply this strategy to the given Site.
+        Returns a dict with nodes,cells which were modified 
+        """
+        assert False
+
+class WallStrategy(Strategy):
+    """ 
+    Add two edges and a new triangle to the forward side of the
+    site.
+    """
+    def metric(self,site):
+        # rough translation from paver
+        theta=site.internal_angle
+        scale_factor = site.edge_length / site.local_length
+
+        # Wall can be applied in a wide variety of situations
+        return 1.0 * (theta > 60*pi/180.) 
+
+    def execute(self,site):
+        na,nb,nc= site.abc
+        grid=site.grid
+        b,c = grid.nodes['x'][ [nb,nc] ]
+        bc=c-b
+        new_x = b + utils.rot(np.pi/3,bc)
+        nd=grid.add_node(x=new_x,fixed=site.af.FREE)
+        new_c=grid.add_cell_and_edges( [nb,nc,nd] )
+
+        return {'nodes': [nd],
+                'cells': [new_c] }
+
+
+class CutoffStrategy(Strategy):
+    def metric(self,site):
+        theta=site.internal_angle
+        scale_factor = site.edge_length / site.local_length
+
+        # Cutoff wants a small-ish internal angle
+        # If the sites edges are long, scale_factor > 1
+        # and we'd like to be making smaller edges, so ideal angle gets smaller
+        # 
+        ideal=60 + (1-scale_factor)*30
+        return np.abs(theta - ideal*pi/180.)
+    def execute(self,site):
+        c=site.grid.add_cell_and_edges( site.abc )
+        return {'cells':[c] }
+
+# copied from paver verbatim
+def one_point_cost(pnt,edges,target_length=5.0):
+    # pnt is intended to complete a triangle with each
+    # pair of points in edges, and should be to the left
+    # of each edge
+    penalty = 0
     
-# when resample nodes on a sliding boundary, want to calculate the available
-# span, and if it's small, start distributing the nodes evenly.
-# where small is defined by local_scale * max_span_factor
-max_span_factor=4     
-def resample(n,anchor,scale,direction):
-    self.log.debug("resample %d to be  %g away from %d in the %s direction"%(n,scale,anchor,
-                                                                             direction) )
-    if direction==1: # anchor to n is t
-        he=self.grid.nodes_to_halfedge(anchor,n)
-    elif direction==-1:
-        he=self.grid.nodes_to_halfedge(n,anchor)
+    max_angle = 85.0*pi/180.
 
-    span_length = free_span(he,max_span_factor*scale,direction)
-    self.log.debug("free span from the anchor is %g"%slidable_length)
+    # all_edges[triangle_i,{ab,bc,ca},{x,y}]
+    all_edges = zeros( (edges.shape[0], 3 ,2), float64 )
+    
+    # get the edges:
+    all_edges[:,0,:] = edges[:,0] - pnt  # ab
+    all_edges[:,1,:] = edges[:,1] - edges[:,0] # bc
+    all_edges[:,2,:] = pnt - edges[:,1] # ca
 
-    if span_length < max_span_factor*scale:
-        n_segments = max(1,round(span_length / scale))
-        target_span = span_length / n_segments
-    else:
-        target_span=scale
-
-    # HERE - move n, deleting anybody in its way, such that the distance between
-    # n and anchor is approximately target_span
+    i = arange(3)
+    im1 = (i-1)%3
+    
+    #--# cost based on angle:
+    abs_angles = arctan2( all_edges[:,:,1], all_edges[:,:,0] )
+    all_angles = (pi - (abs_angles[:,i] - abs_angles[:,im1]) % (2*pi)) % (2*pi)
         
-self=af
+    # a_angles = (pi - (ab_angles - ca_angles) % (2*pi)) % (2*pi)
+    # b_angles = (pi - (bc_angles - ab_angles) % (2*pi)) % (2*pi)
+    # c_angles = (pi - (ca_angles - bc_angles) % (2*pi)) % (2*pi)
 
-local_length = self.scale( points.mean(axis=0) )
+    if 1:
+        # 60 is what it's been for a while, but I think in one situation
+        # this put too much weight on small angles.
+        # tried considering just large angles, but that quickly blew up.
+        # even just changing this to 50 still blows up.
+        #  how about a small tweak - s/60/58/ ??
+        worst_angle = abs(all_angles - 60*pi/180.).max() 
+        alpha = worst_angle /(max_angle - 60*pi/180.0)
 
-# def resample_neighbors(self,site):
-a,b,c = site.abc
-for n,direction in [ (a,-1),
-                     (c,1) ]:
-    if ( (self.grid.nodes['fixed'][n] == self.SLIDE) and
-         len(self.grid.node_to_edges(n))<=2 ):
-        resample(n=n,anchor=b,scale=local_length,direction=direction)
+        # 10**alpha: edges got very short...
+        # 5**alpha - 1: closer, but overall still short edges.
+        # alpha**5: angles look kind of bad
+        angle_penalty = 10*alpha**5
+
+        # Seems like it doesn't try hard enough to get rid of almost bad angles.
+        # in one case, there is a small angle of 33.86 degrees, and another angle
+        # of 84.26 degrees. so the cost function only cares about the small angle
+        # because it is slightly more deviant from 60deg, but we may be in a cell
+        # where the only freedom we have is to change the larger angles.
+
+        # so add this in:
+        if 1:
+            # extra exponential penalty for nearly bad triangles:
+            # These values mean that 3 degrees before the triangle is invalid
+            # the exponential cuts in and will add a factor of e by the time the
+            # triangles is invalid.
+
+            scale_rad = 3.0*pi/180. # radians - e-folding scale of the cost
+            # max_angle - 2.0*scale_rad works..
+            thresh = max_angle - 1.0*scale_rad # angle at which the exponential 'cuts in'
+            big_angle_penalty = exp( (all_angles.max() - thresh) / scale_rad)
+    else:
+        alphas = (all_angles - 60*pi/180.) / (max_angle - 60*pi/180.)
+        alphas = 10*alphas**4
+        angle_penalty = alphas.sum()
+    
+    penalty += angle_penalty + big_angle_penalty
+
+    #--# Length penalties:
+    ab_lens = (all_edges[:,0,:]**2).sum(axis=1)
+    ca_lens = (all_edges[:,2,:]**2).sum(axis=1)
+
+    if 1:  # the usual..
+        min_len = min(ab_lens.min(),ca_lens.min())
+        max_len = max(ab_lens.min(),ca_lens.min())
+
+        undershoot = target_length**2 / min_len
+        overshoot  = max_len / target_length**2
+
+        length_penalty = 0
+
+        length_factor = 2
+        length_penalty += length_factor*(max(undershoot,1) - 1)
+        length_penalty += length_factor*(max(overshoot,1) - 1)
+    elif 1:
+        # Try an exponential
+        rel_len_ab = ab_lens / target_length**2
+        rel_len_ca = ca_lens / target_length**2
+
+        # So we want to severely penalize things that are more than double
+        # the target length or less than half the target length.
+        # just a wild guess here, that maybe the threshold needs to be larger.
+        # well, how about the penalty kicks in at 3x
+        thresh = 9.0 # 2.5*2.5
+        length_penalty = exp( rel_len_ab - thresh ).sum() + exp( 1.0/rel_len_ab - thresh).sum()
+        length_penalty += exp( rel_len_ca - thresh ).sum() + exp( 1.0/rel_len_ca - thresh).sum()
+
+    else:
+        rel_errs_ab = (ab_lens - target_length**2) / target_length**2
+        rel_errs_ca = (ca_lens - target_length**2) / target_length**2
+
+        length_penalty = ( (rel_errs_ab**2).sum() + (rel_errs_ca**2).sum() )
+        
+    penalty += length_penalty
+
+    return penalty
+    
+Wall=WallStrategy()
+Cutoff=CutoffStrategy()
+
+def cost_function(self,n):
+    local_length = self.scale( self.grid.nodes['x'][n] )
+    my_cells = self.grid.node_to_cells(n)
+
+    if len(my_cells) == 0:
+        return None
+
+    cell_nodes = [self.grid.cell_to_nodes(c)
+                  for c in my_cells ]
+    
+    # for the moment, can only deal with triangles
+    cell_nodes=np.array(cell_nodes)
+    
+    # pack our neighbors from the cell list into an edge
+    # list that respects the CCW condition that pnt must be on the
+    # left of each segment
+    for j in range(len(cell_nodes)):
+        if cell_nodes[j,0] == n:
+            cell_nodes[j,:2] = cell_nodes[j,1:]
+        elif cell_nodes[j,1] == n:
+            cell_nodes[j,1] = cell_nodes[j,0]
+            cell_nodes[j,0] = cell_nodes[j,2]
+
+    edges = cell_nodes[:,:2]
+    edge_points = self.grid.nodes['x'][edges]
+
+    def cost(x):
+        return one_point_cost(x,edge_points,target_length=local_length)
+    
+    return cost
 
 
+def optimize(self,edits):
+    nodes = edits.get('nodes',[])
+    for c in edits.get('cells',[]):
+        for n in self.grid.cell_to_nodes(c):
+            if n not in nodes:
+                nodes.append(n)
+    for n in nodes:
+        relax_node(self,n)
+    
+def relax_node(self,n):
+    if self.grid.nodes['fixed'][n] == self.FREE:
+        relax_free_node(self,n)
+    else:
+        print "Not ready for relaxing non-free nodes"
+        
+def relax_free_node(self,n0):
+    cost=cost_function(self,n)
+    if cost is None:
+        return 
+    x0=self.grid.nodes['x'][n]
+    local_length=self.scale( x0 )
+    new_x = opt.fmin(cost,
+                     x0,
+                     xtol=local_length*1e-4,
+                     disp=0)
+    dx=utils.dist( new_x - x0 )
+    print dx
+    self.grid.modify_node(n,x=new_x)
+
+
+af=test_basic_setup()
+
+site=af.choose_site()
+af.resample_neighbors(site)
+edits = Wall.execute(site)
+optimize(af,edits)
 
 ## 
-def wall():
-    theta=site.internal_angle()
-    points=site.points()
-edge_length = utils.dist( np.diff(points,axis=0) ).mean()
-scale_factor = edge_length / local_length
+site2=af.choose_site()
+af.resample_neighbors(site2)
+edits=Cutoff.execute(site2)
+optimize(af,edits)
 
+##
+
+# HERE:
+#   So it can do a wall, a cutoff, and optimize them
+#   correctly.
+#   Need to think about how these pieces are going to work together
+#   And probably a good time to (a) move most of the code above into front.py
+#   and (b) start adding the rollback, graph search side of things.  Without
+#   a solid CDT, it's going to be a pita, but at least we can have the logic
+#   there.
+
+site3=af.choose_site()
+
+if plt:
+    plt.clf()
+    af.grid.plot_edges()
+
+    coll=af.grid.plot_halfedges(values=af.grid.edges['cells'])
+    coll.set_lw(0)
+    coll.set_cmap('winter')
+    site3.plot()
