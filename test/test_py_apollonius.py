900d1bba93102a2e76cdeb54c0fad01bdae6517c
Rusty Holleman <rustychris@gmail.com>
2017-10-31 21:00:40 -0700

    Force error if apollonius used but not supported.

--- test/test_py_apollonius.py
+++ test/test_py_apollonius.py
@@ -1,3 +1,4 @@
+import numpy as np
 from stompy.spatial import field
 
 def test_basic():
@@ -8,3 +9,14 @@ def test_basic():
 
     g=pa.to_grid(100,100)
 
+##
+
+def test_larger():
+    # About 1.5s on basic macbook
+    pa=field.PyApolloniusField()
+
+    for it in range(1000):
+        xy=1000*np.random.random(2)
+        pa.insert(xy,10)
+
+    g=pa.to_grid(200,200)
