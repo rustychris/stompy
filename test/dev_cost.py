4301658bac263b465ce8aaf765a36c4e5d319341
Rusty Holleman <rustychris@gmail.com>
2017-12-01 11:34:55 -0600

    progress on circumcenter-based optimization

--- test/dev_cost.py
+++ test/dev_cost.py
@@ -1,4 +1,5 @@
 import os
+import math
 import time
 import logging
 import matplotlib.pyplot as plt
@@ -105,12 +106,79 @@ def circumcenter(p1,p2,p3):
     
     return vc
 
+def circumcenter_py(p1,p2,p3):
+    """ no longer vectorized
+    """
+    ref = p1
+    
+    p1x = 0
+    p1y = 0
+    p2x = p2[0] - ref[0]
+    p2y = p2[1] - ref[1]
+    p3x = p3[0] - ref[0]
+    p3y = p3[1] - ref[1]
+
+    # taken from TRANSFORMER_gang.f90
+    dd=2.0*((p1x-p2x)*(p1y-p3y) -(p1x-p3x)*(p1y-p2y))
+    b_com=p1x*p1x+p1y*p1y
+    b1=b_com-p2x*p2x-p2y*p2y
+    b2=b_com-p3x*p3x-p3y*p3y 
+
+    return [ (b1*(p1y-p3y)-b2*(p1y-p2y))/dd + ref[0] ,
+             (b2*(p1x-p2x)-b1*(p1x-p3x))/dd + ref[1] ]
+
+
+
+def gen_circum_partial(p1,p2):
+    # return a function which takes a single point which completes
+    # all triangles described by the array of vertices in p1 and p2,
+    # and returns respective circumcenters.
+    # furthermore, p3 is taken as a single point, while p1 and
+    # p2 are arrays of points
+    ref = p1
+    p1x = 0.0
+    p1y = 0.0
+    p2x = p2[:,0] - ref[:,0]
+    p2y = p2[:,1] - ref[:,1]
+
+    p12dx=p1x-p2x
+    p12dy=p1y-p2y
+
+    dd0=2*p12dx*p1y - 2*p12dy*p1x 
+    dd_f_x=  2*p12dy
+    dd_f_y= -2*p12dx
+    b1=p1x**2+p1y**2-p2x**2-p2y**2
+    b20=p1x**2+p1y**2
+    
+    def partial(p3):
+        p3x = p3[0] - ref[:,0]
+        p3y = p3[1] - ref[:,1]
+
+        vc = np.zeros( p1.shape, np.float64)
+
+        # taken from TRANSFORMER_gang.f90
+        # original:
+        # dd=2.0*(p12dx*(p1y-p3y) - p12dy*(p1x-p3x))
+        # distributed:
+        # dd=2*p12dx*p1y - 2*p12dx*p3y - 2*p12dy*p1x + 2*p12dy*p3x
+        dd=dd0 + dd_f_x*p3x + dd_f_y*p3y
+        
+        # b2=p1x**2+p1y**2-p3x**2-p3y**2
+        b2=b20-p3x*p3x-p3y*p3y
+        
+        vc[:,0]=(b1*(p1y-p3y)-b2*(p1y-p2y))/dd + ref[:,0]
+        vc[:,1]=(b2*(p1x-p2x)-b1*(p1x-p3x))/dd + ref[:,1]
+
+        return vc
+    return partial
+
+
 ##
 
 class TestCC(front.AdvancingTriangles):
     """ Testing other options for node cost
     """
-    def cost_function(self,n):
+    def cost_function(self,n,method='cc_py'):
         local_length = self.scale( self.grid.nodes['x'][n] )
         my_cells = self.grid.node_to_cells(n)
 
@@ -139,8 +207,20 @@ class TestCC(front.AdvancingTriangles):
         def cost(x,edge_points=edge_points,local_length=local_length):
             return front.one_point_cost(x,edge_points,target_length=local_length)
 
+        circum_partial=gen_circum_partial( edge_points[:,0,:],edge_points[:,1,:] )
+
+        Alist=[ [ e[0],e[1] ]
+                for e in edge_points[:,0,:] ]
+        Blist=[ [ e[0],e[1] ]
+                for e in edge_points[:,1,:] ]
+        EPS=1e-5*local_length
+
         def cost_cc_and_scale(x0):
-            tri_cc = circumcenter( edge_points[:,0,:],edge_points[:,1,:],x0[None,:] )
+            # tri_cc = circumcenter( edge_points[:,0,:],edge_points[:,1,:],x0[None,:] )
+            # tri_cc = circum_partial(x0)
+            x0l=list(x0)
+            tri_cc=np.array( [ circumcenter_py(A,B,x0l)
+                               for A,B in zip(Alist,Blist)] )
 
             As=edge_points[:,0,:]
             Bs=edge_points[:,1,:]
@@ -164,96 +244,118 @@ class TestCC(front.AdvancingTriangles):
             vecCA=CAs/magCAs[...,None]
             leftCA=vecCA[:,0]*deltaCA[:,1] - vecCA[:,1]*deltaCA[:,0]
 
-            EPS=1e-5*local_length
             cc_cost = ( (1./leftAB.clip(EPS,np.inf)).sum() +
                         (1./leftBC.clip(EPS,np.inf)).sum() +
                         (1./leftCA.clip(EPS,np.inf)).sum() )
+            # non-dimensionalize
+            cc_cost *= local_length
 
             scale_cost=(magABs-local_length)**2 + (magBCs-local_length)**2 + (magCAs-local_length)**2
-            scale_cost=scale_cost.sum() / local_length*local_length
+            scale_cost=scale_cost.sum() / (local_length*local_length)
 
+            return 0.1*cc_cost+scale_cost
+
+        def cost_cc_and_scale_py(x0):
+            C=list(x0)
+            cc_cost=0
+            scale_cost=0
+            
+            for A,B in zip(Alist,Blist):
+                tri_cc=circumcenter_py(A,B,C)
+
+                #As=edge_points[:,0,:]
+                #Bs=edge_points[:,1,:]
+                #Cs=x0[None,:]
+
+                deltaAB=[ tri_cc[0] - A[0],
+                          tri_cc[1] - A[1]]
+                ABs=[B[0]-A[0],B[1]-A[1]]
+                magABs=math.sqrt( ABs[0]*ABs[0] + ABs[1]*ABs[1])
+                vecAB=[ABs[0]/magABs, ABs[1]/magABs]
+                leftAB=vecAB[0]*deltaAB[1] - vecAB[1]*deltaAB[0] 
+
+                deltaBC=[tri_cc[0] - B[0],
+                         tri_cc[1] - B[1]]
+                BCs=[C[0]-B[0], C[1]-B[1]]
+                magBCs=math.sqrt( BCs[0]*BCs[0] + BCs[1]*BCs[1] )
+                vecBC=[BCs[0]/magBCs, BCs[1]/magBCs]
+                leftBC=vecBC[0]*deltaBC[1] - vecBC[1]*deltaBC[0]
+
+                deltaCA=[tri_cc[0] - C[0],
+                         tri_cc[1] - C[1]]
+                CAs=[A[0]-C[0],A[1]-C[1]]
+                magCAs=math.sqrt(CAs[0]*CAs[0] + CAs[1]*CAs[1])
+                vecCA=[CAs[0]/magCAs, CAs[1]/magCAs]
+                leftCA=vecCA[0]*deltaCA[1] - vecCA[1]*deltaCA[0]
+
+                # reciprocal means that we have keep it strictly
+                # positive, which in turn means that the optimization
+                # can't escape a case where the cc is outside the cell
+                # cc_cost += 0.2*local_length*( 1./max(EPS,leftAB) +
+                #                           1./max(EPS,leftBC) +
+                #                           1./max(EPS,leftCA) )
+                # cc_fac=-4. # not bad
+                cc_fac=-2.
+                cc_cost += ( math.exp(cc_fac*leftAB/local_length) +
+                             math.exp(cc_fac*leftBC/local_length) +
+                             math.exp(cc_fac*leftCA/local_length) )
+                
+
+                scale_cost+=(magABs-local_length)**2 + (magBCs-local_length)**2 + (magCAs-local_length)**2
+
+            scale_cost /= local_length*local_length
             return cc_cost+scale_cost
-        # return cost
-        return cost_cc_and_scale
+
+        if method=='base':
+            return cost
+        elif method=='cc':
+            return cost_cc_and_scale
+        elif method=='cc_py':
+            return cost_cc_and_scale_py
+        else:
+            assert False
 ##
 
-# Get a grid with some candidates for tuning
-# af=test_basic_setup(TestCC)
-af=test_basic_setup(front.AdvancingTriangles)
-t=time.time()
-af.loop()
-elapsed_base=time.time() - t
-# 8.5s for 172 cells.  20.1 cells/s
+# # Get a grid with some candidates for tuning
+# # af=test_basic_setup(TestCC)
+# af=test_basic_setup(front.AdvancingTriangles)
+# t=time.time()
+# af.loop()
+# elapsed_base=time.time() - t
+# # 8.5s for 172 cells.  20.1 cells/s
 
-## 
 af2=test_basic_setup(TestCC)
 t=time.time()
 af2.loop()
 elapsed_cc=time.time() - t
 
+print "%.2f cells/s"%( af2.grid.Ncells() / elapsed_cc)
 # 15.2s for 148 cells.  9.6 cells/s
-
-## 
-af.cdt.post_check=False
-
-af.current=af.root=front.DTChooseSite(af)
-
-for _ in range(400):
-    if not af.current.children:
-        break # we're done?
-
-    for child_i in range(len(af.current.children)):
-        if af.current.try_child(child_i):
-            # Accept the first child which returns true
-            break
-    else:
-        assert False # none of the children worked out
+# 14.2s with turning circumcenter into a partial
+# Where is the time?
+# 15.8s, and 10.3 is in cost_cc_and_scale
+# 2.2s in partial(), vs. 3.3 in circumcenter vs 0.9 in cirumcenter_py
+# how much faster would it be to use straight python
+# about as fast as old code, but has a bug -- actually old code had a bug.
+# with more intentional scaling of cc_cost - makes an acceptable grid
+# 130 cells (method=cc), 11.4 cells/s
+# pure python gets 20.5 cells/s
 
 ##
 
 plt.figure(2).clf()
-af.grid.plot_edges(lw=1.0)
-# af.grid.plot_nodes(labeler=lambda i,r: str(i))
+af2.grid.plot_edges(lw=1.0)
+# af2.grid.plot_nodes(labeler=lambda i,r: str(i))
 
 ## 
 # choose a free node with a few neighbors:
-n=37
+n=34
 
-# def cost_function(self,n):
-self=af
-if 1:
-    local_length = self.scale( self.grid.nodes['x'][n] )
-    my_cells = self.grid.node_to_cells(n)
+cost=af2.cost_function(n)
+print "cc_py: ",cost(af2.grid.nodes['x'][n])
 
-    assert len(my_cells) != 0
-
-    cell_nodes = [self.grid.cell_to_nodes(c)
-                  for c in my_cells ]
-
-    # for the moment, can only deal with triangles
-    cell_nodes=np.array(cell_nodes)
-
-    # pack our neighbors from the cell list into an edge
-    # list that respects the CCW condition that pnt must be on the
-    # left of each segment
-    for j in range(len(cell_nodes)):
-        if cell_nodes[j,0] == n:
-            cell_nodes[j,:2] = cell_nodes[j,1:]
-        elif cell_nodes[j,1] == n:
-            cell_nodes[j,1] = cell_nodes[j,0]
-            cell_nodes[j,0] = cell_nodes[j,2] # otherwise, already set
-
-    edges = cell_nodes[:,:2]
-    edge_points = self.grid.nodes['x'][edges]
-
-    def cost(x,edge_points=edge_points,local_length=local_length):
-        return front.one_point_cost(x,edge_points,target_length=local_length)
-
-## 
-
-cost=af.cost_function(37)
-
-cost(af.grid.nodes['x'][37])
+cost=af2.cost_function(n,method='cc')
+print "cc: ",cost(af2.grid.nodes['x'][n])
 
 ## 
 x0=af.grid.nodes['x'][n]
