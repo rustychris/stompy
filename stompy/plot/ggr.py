1a6eb92cef70011e45cdf83c78486b29484e0012
Rusty Holleman <rustyh@sfei.org>
2017-12-19 11:48:31 -0800

    Update and remove DOS encoding

--- stompy/plot/ggr.py
+++ stompy/plot/ggr.py
@@ -20,8 +20,11 @@ class GimpGradient:
     def read(self, f):
         """ Read a .ggr file from f (either an open file or a file path).
         """
-        if isinstance(f, basestring):
-            f = file(f)
+        try:
+            f=open(f)
+        except TypeError:
+            pass # probably f is already a file
+        
         if f.readline().strip() != "GIMP Gradient":
             raise Exception("Not a GIMP gradient file")
         line = f.readline().strip()
@@ -36,7 +39,7 @@ class GimpGradient:
             (seg.l, seg.m, seg.r,
                 seg.rl, seg.gl, seg.bl, _,
                 seg.rr, seg.gr, seg.br, _,
-             seg.fn, seg.space) = map(float, line.split())[:13]
+             seg.fn, seg.space) = list(map(float, line.split()))[:13]
             self.segs.append(seg)
             
     def color(self, x):
@@ -95,52 +98,3 @@ class GimpGradient:
                 vl + (vr-vl) * f
                 )
         return c
-    
-if __name__ == '__main__':
-    import sys, wx
-
-    class GgrView(wx.Frame):
-        def __init__(self, ggr, chunks):
-            """ Display the ggr file as a strip of colors.
-                If chunks is non-zero, then also display the gradient quantized
-                into that many chunks.
-            """
-            super(GgrView, self).__init__(None, -1, 'Ggr: %s' % ggr.name)
-            self.ggr = ggr
-            self.chunks = chunks
-            self.SetSize((600, 100))
-            self.panel = wx.Panel(self)
-            self.panel.Bind(wx.EVT_PAINT, self.on_paint)
-            self.panel.Bind(wx.EVT_SIZE, self.on_size)
-
-        def on_paint(self, event):
-            dc = wx.PaintDC(self.panel)
-            cw, ch = self.GetClientSize()
-            if self.chunks:
-                self.paint_some(dc, 0, 0, ch/2)
-                self.paint_some(dc, self.chunks, ch/2, ch)
-            else:
-                self.paint_some(dc, 0, 0, ch)
-                
-        def paint_some(self, dc, chunks, y0, y1):
-            cw, ch = self.GetClientSize()
-            chunkw = 1
-            if chunks:
-                chunkw = (cw // chunks) or 1
-            for x in range(0, cw, chunkw):
-                c = map(lambda x:int(255*x), ggr.color(float(x)/cw))
-                dc.SetPen(wx.Pen(wx.Colour(*c), 1))
-                dc.SetBrush(wx.Brush(wx.Colour(*c), wx.SOLID))
-                dc.DrawRectangle(x, y0, chunkw, y1-y0)
-        
-        def on_size(self, event):
-            self.Refresh()
-
-    app = wx.PySimpleApp()
-    ggr = GimpGradient(sys.argv[1])
-    chunks = 0
-    if len(sys.argv) > 2:
-        chunks = int(sys.argv[2])
-    f = GgrView(ggr, chunks)
-    f.Show()
-    app.MainLoop()
