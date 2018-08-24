4f46250618877e5363965157b640d9256b2694ee
Rusty Holleman <rustychris@gmail.com>
2018-08-24 15:00:46 -0700

    time slices, bug fixes

--- stompy/model/delft/dfm_to_ptm.py
+++ stompy/model/delft/dfm_to_ptm.py
@@ -46,6 +46,7 @@ double h_flow_avg(nMesh2_edge=76593, nMesh2_layer_3d=54, nMesh2_data_time=483);
 This is the average volume flux at an edge (each j,k) over the preceeding time interval.
 """
 ##
+import sys
 import six
 import os
 import glob
@@ -61,6 +62,8 @@ import stompy.model.delft.waq_scenario as waq
 
 class DFlowToPTMHydro(object):
     overwrite=False
+    time_slice=slice(None)
+
     def __init__(self,mdu_path,output_fn,**kwargs):
         self.__dict__.update(kwargs)
 
@@ -85,7 +88,6 @@ class DFlowToPTMHydro(object):
         self.initialize_output()
         try:
             self.initialize_output_variables()
-            
             self.write_time_steps()
         finally:
             # helps netCDF4 release the dataset and not block
@@ -100,30 +102,39 @@ class DFlowToPTMHydro(object):
         """
         # incoming dataset from DFM:
         self.map_ds=xr.open_dataset(self.map_file)
+        # Additionally trim to subset of times here:
+        subset_ds=self.map_ds.isel(time=self.time_slice)
+
         # shallow copy of that with renames for PTM compatibility
-        self.mod_map_ds=self.map_ds.rename({'time':'nMesh2_data_time',
-                                            'nmesh2d_face':'nMesh2_face',
-                                            'nmesh2d_edge':'nMesh2_edge',
-                                            'nmesh2d_node':'nMesh2_node',
-                                            'max_nmesh2d_face_nodes':'nMaxMesh2_face_nodes',
-                                            'mesh2d_face_x':'Mesh2_face_x',
-                                            'mesh2d_face_y':'Mesh2_face_y',
-                                            'mesh2d_edge_x':'Mesh2_edge_x',
-                                            'mesh2d_edge_y':'Mesh2_edge_y'
+        self.mod_map_ds=subset_ds.rename({'time':'nMesh2_data_time',
+                                          'nmesh2d_face':'nMesh2_face',
+                                          'nmesh2d_edge':'nMesh2_edge',
+                                          'nmesh2d_node':'nMesh2_node',
+                                          'max_nmesh2d_face_nodes':'nMaxMesh2_face_nodes',
+                                          'mesh2d_face_x':'Mesh2_face_x',
+                                          'mesh2d_face_y':'Mesh2_face_y',
+                                          'mesh2d_edge_x':'Mesh2_edge_x',
+                                          'mesh2d_edge_y':'Mesh2_edge_y'
         })
+
         self.g=unstructured_grid.UnstructuredGrid.from_ugrid(self.map_ds)
+        # flip edges to keep invariant that external cells are always
+        # second.
+        e2c=self.g.edge_to_cells()
+        to_flip=e2c[:,0]<0
+        for fld in ['nodes','cells']:
+            a=self.g.edges[fld][to_flip][:,0].copy()
+            b=self.g.edges[fld][to_flip][:,1].copy()
+            self.g.edges[fld][to_flip,0] = b
+            self.g.edges[fld][to_flip,1] = a
+
+        self.flipped=to_flip
 
     def template_ds(self):
         """
         Construct an xarray dataset with static geometry and basic
         dimensions.
         """
-        # flip edges to keep invariant that external cells are always
-        # second.
-        e2c=self.g.edge_to_cells()
-        to_flip=e2c[:,0]<0
-        for fld in ['nodes','cells']:
-            self.g.edges[fld][to_flip] = self.g.edges[fld][to_flip][:,::-1]
 
         out_ds=self.g.write_to_xarray(mesh_name="Mesh2",
                                       node_coordinates="Mesh2_node_x Mesh2_node_y",
@@ -390,18 +401,20 @@ class DFlowToPTMHydro(object):
         # method maybe consolidates time step selection locations.
         self.write_time_strings()
 
+        # "safe" versions of the cells on either side of an edge
+        c1=e2c[:,0].copy() ; c2=e2c[:,1].copy()
+        c2[c2<0]=c1[c2<0]
+        c1[c1<0]=c2[c1<0] # unnecessary, but hey..
+
         for ti,t in enumerate(times):
             if True: # ti%24==0:
                 print("%d/%d t=%s"%(ti,len(times),t))
 
             # try setting edges to be dry by k_top=0.  this is specific to nk=1
-            cell_water_depth=self.map_ds.mesh2d_waterdepth.isel(time=ti)
+            cell_water_depth=self.mod_map_ds.mesh2d_waterdepth.isel(nMesh2_data_time=ti)
             # all or nothing for sigma layers
             self.cell_k_top_var[:,ti] = np.where(cell_water_depth>0,self.nkmax,0)
             # edge eta is taken from the higher freesurface
-            c1=e2c[:,0] ; c2=e2c[:,1]
-            c2[c2<0]=c1[c2<0]
-            c1[c1<0]=c2[c1<0] # unnecessary, but hey..
             eta_cell=self.out_nc['Mesh2_sea_surface_elevation'][:,ti]
             edge_eta=np.maximum( eta_cell[c1], eta_cell[c2] )
             edge_water_depth=edge_eta + self.out_nc['Mesh2_edge_depth'][:]
@@ -509,255 +522,17 @@ class DFlowToPTMHydro(object):
 # [Qa, Qb, Qc, Qd, Qd].  I don't understand what that's about.
 # look at cell 18, around time index 200, 201
 
-mdu_path="/home/rusty/src/csc/dflowfm/runs/20180807_grid97_04_ptm/flowfm.mdu"
+if 0:
+    # Testing
+    mdu_path="/home/rusty/src/csc/dflowfm/runs/20180807_grid97_04_ptm/flowfm.mdu"
+    converter=DFlowToPTMHydro(mdu_path,'dfm_ptm_hydro3.nc')
 
-DFlowToPTMHydro(mdu_path,'dfm_ptm_hydro2.nc')
-
-##
-
-## Matt Rayson's code:
-#
-## Dimensions with hard-wired values
-#other_dims = {\
-#        'Three':3,\
-#        'Two':2,\
-#        'nsp':1,\
-#        'date_string_length':19,\
-#        'nMesh2_time':1\
-#        }
-#
-#            'h_flow_avg',\
-#            'v_flow_avg',\
-#            'Mesh2_edge_wet_area',\
-#            'Mesh2_face_wet_area',\
-#            'Mesh2_edge_bottom_layer',\
-#            'Mesh2_edge_top_layer',\
-#            'Mesh2_face_bottom_layer',\
-#            'Mesh2_face_top_layer',\
-#            'Mesh2_face_water_volume',\
-#
-#
-#FILLVALUE=-9999
-#
-#def suntans2untrim(ncfile,outfile,tstart,tend,grdfile=None):
-#    """
-#    Converts a suntans averages netcdf file into untrim format
-#    for use in particle tracking
-#    """
-#    ####
-#    # Step 1: Load the suntans data object
-#    ####
-#    print "ncfile",ncfile
-#    sun = Spatial(ncfile,klayer=[-99])
-#
-#    # Calculate some other variables
-#    sun.de = sun.get_edgevar(sun.dv,method='min')
-#    sun.mark[sun.mark==5]=0
-#    sun.mark[sun.mark==3]=2
-#    sun.facemark = np.zeros((sun.Nc,),dtype=np.int)
-#
-#    # Update the grad variable from the ascii grid file if supplied
-#    if not grdfile == None:
-#        print 'Updating grid with ascii values...'
-#        grd = Grid(grdfile)
-#        sun.grad = grd.grad[:,::-1]
-#
-#    ###
-#    # Step 2: Write the grid variables to a netcdf file
-#    ###
-#    nc = Dataset(outfile,'w',format='NETCDF4_CLASSIC')
-#
-#    # Global variable
-#    nc.Description = 'UnTRIM history file converted from SUNTANS output'
-#
-#    # Write the dimensions
-#    for dd in untrim_griddims.keys():
-#        if dd == 'time':
-#            nc.createDimension(untrim_griddims[dd],0)
-#        elif dd =='numsides':
-#            nc.createDimension(untrim_griddims[dd],sun.maxfaces)
-#        else:
-#            nc.createDimension(untrim_griddims[dd],sun[dd])
-#
-#
-#    for dd in other_dims:
-#        nc.createDimension(dd,other_dims[dd])
-#
-#    ###
-#    # Step 3: Initialize all of the grid variables
-#    ###
-#    def create_nc_var(name, dimensions, attdict,data=None, \
-#        dtype='f8',zlib=False,complevel=0,fill_value=999999.0):
-#            
-#        tmp=nc.createVariable(name, dtype, dimensions,\
-#            zlib=zlib,complevel=complevel,fill_value=fill_value)
-#
-#        for aa in attdict.keys():
-#            tmp.setncattr(aa,attdict[aa])
-#        
-#        if not data==None:
-#            nc.variables[name][:] = data
-#     
-#    # Make sure the masked cells have a value of -1
-#    mask = sun['cells'].mask.copy()
-#    sun['cells'][mask]=FILLVALUE
-#    sun['face'][mask]=FILLVALUE
-#
-#    for vv in untrim_gridvars.keys():
-#        vname = untrim_gridvars[vv]
-#        print 'Writing grid variable %s (%s)...'%(vname,vv)
-#
-#        if vv=='time':
-#            continue
-#
-#        # add dz_min attribute to z_r variable
-#        if vv == 'z_r':
-#            ugrid[vname]['attributes'].update({'dz_min':1e-5})
-#            #sun[vv][:]=sun[vv][::-1]
-#            sun[vv][:]=sun['z_w'][0:-1][::-1]
-#
-#        # Reverse the order of grad(???)
-#        if vv=='grad':
-#            sun[vv][:]=sun[vv][:,::-1]
-#
-#        ## Fix one-based indexing
-#        #if vv in ['cells','edges','grad']:
-#        #    mask = sun[vv][:]==-1
-#        #    tmp = sun[vv][:]+1
-#        #    tmp[mask]=-1
-#        #    #sun[vv][:]=sun[vv][:]+1
-#        #    create_nc_var(vname,ugrid[vname]['dimensions'],ugrid[vname]['attributes'],\
-#        #        data=tmp,dtype=ugrid[vname]['dtype'])
-#
-#        create_nc_var(vname,ugrid[vname]['dimensions'],ugrid[vname]['attributes'],\
-#            data=sun[vv],dtype=ugrid[vname]['dtype'])
-#
-#            
-#    # Initialize the two time variables
-#    vname=untrim_gridvars['time']
-#    create_nc_var(vname,ugrid[vname]['dimensions'],ugrid[vname]['attributes'],\
-#            dtype=ugrid[vname]['dtype'])
-#    vname = 'Mesh2_data_time_string'
-#    create_nc_var(vname,ugrid[vname]['dimensions'],ugrid[vname]['attributes'],\
-#            dtype=ugrid[vname]['dtype'])
-#
-#    ###
-#    # Step 4: Initialize all of the time-varying variables (but don't write) 
-#    ###
-#    for vname  in varnames:
-#        print 'Creating variable %s...'%(vname)
-#
-#        create_nc_var(vname,ugrid[vname]['dimensions'],ugrid[vname]['attributes'],\
-#            dtype=ugrid[vname]['dtype'],zlib=True,complevel=1,fill_value=999999.)
-#
-#    ###
-#    # Step 5: Loop through all of the time steps and write the variables
-#    ###
-#    tsteps = sun.getTstep(tstart,tend)
-#    tdays = othertime.DaysSince(sun.time,basetime=datetime(1899,12,31))
-#    for ii, tt in enumerate(tsteps):
-#        # Convert the time to the untrim formats
-#        timestr = datetime.strftime(sun.time[tt],'%Y-%m-%d %H:%M:%S')
-#
-#        print 'Writing data at time %s (%d of %d)...'%(timestr,tt,tsteps[-1]) 
-#
-#        #Write the time variables
-#        nc.variables['Mesh2_data_time'][ii]=tdays[ii]
-#        nc.variables['Mesh2_data_time_string'][:,ii]=timestr
-#
-#        # Load each variable or calculate it and convert it to the untrim format
-#        sun.tstep=[tt]
-#
-#        ###
-#        # Compute a few terms first
-#        eta = sun.loadData(variable='eta' )  
-#        U = sun.loadData(variable='U_F' )  
-#        #U = sun.loadData(variable='U' )  
-#        dzz = sun.getdzz(eta)
-#        dzf = sun.getdzf(eta)
-#
-#        
-#        vname='Mesh2_sea_surface_elevation'
-#        #print '\tVariable: %s...'%vname
-#        nc.variables[vname][:,ii]=eta
-#
-#        vname = 'Mesh2_salinity_3d'
-#        #print '\tVariable: %s...'%vname
-#        tmp3d = sun.loadData(variable='salt' )  
-#        nc.variables[vname][:,:,ii]=tmp3d.swapaxes(0,1)[:,::-1]
-#
-#        vname = 'Mesh2_vertical_diffusivity_3d'
-#        #print '\tVariable: %s...'%vname
-#        tmp3d = sun.loadData(variable='nu_v' )  
-#        nc.variables[vname][:,:,ii]=tmp3d.swapaxes(0,1)[:,::-1]
-#
-#        vname = 'h_flow_avg'
-#        #print '\tVariable: %s...'%vname
-#        nc.variables[vname][:,:,ii]=U.swapaxes(0,1)[:,::-1]
-#
-#
-#        vname = 'v_flow_avg'
-#        #print '\tVariable: %s...'%vname
-#        tmp3d = sun.loadData(variable='w' ) * sun.Ac # m^3/s
-#        nc.variables[vname][:,:,ii]=tmp3d.swapaxes(0,1)[:,::-1]
-#
-#        # Need to calculate a few terms for the other variables
-#
-#        vname = 'Mesh2_edge_wet_area'
-#        #print '\tVariable: %s...'%vname
-#        #dzf = sun.loadData(variable='dzf')
-#        tmp3d = dzf*sun.df
-#        nc.variables[vname][:,:,ii]=tmp3d.swapaxes(0,1)[:,::-1]
-#
-#        vname = 'Mesh2_face_water_volume'
-#        #print '\tVariable: %s...'%vname
-#        #dzz = sun.loadData(variable='dzz')
-#        tmp3d = dzz*sun.Ac
-#        nc.variables[vname][:,:,ii]=tmp3d.swapaxes(0,1)[:,::-1]
-#
-#        vname = 'Mesh2_face_wet_area'
-#        #print '\tVariable: %s...'%vname
-#        tmp3d = np.repeat(sun.Ac[np.newaxis,...],sun.Nkmax,axis=0)
-#        nc.variables[vname][:,:,ii]=tmp3d.swapaxes(0,1)[:,::-1]
-#
-#        # UnTRIM references from bottom to top i.e.
-#        # k = 0 @ bed ; k = Nkmax-1 @ top
-#
-#        vname = 'Mesh2_edge_bottom_layer'
-#        #print '\tVariable: %s...'%vname
-#        #tmp2d = sun.Nkmax-sun.Nke # zero based
-#        tmp2d = sun.Nkmax-sun.Nke+1 # one based
-#        nc.variables[vname][:,ii]=tmp2d
-#
-#        vname = 'Mesh2_edge_top_layer'
-#        #print '\tVariable: %s...'%vname
-#        etop = sun.loadData(variable='etop')
-#        #tmp2d = sun.Nkmax-etop-1 # zero based
-#        tmp2d = sun.Nkmax-etop # one based
-#        nc.variables[vname][:,ii]=tmp2d
-#
-#        vname = 'Mesh2_face_bottom_layer'
-#        #print '\tVariable: %s...'%vname
-#        #tmp2d = sun.Nkmax-sun.Nk + 1 # zero based
-#        tmp2d = sun.Nkmax-sun.Nk # one based
-#        nc.variables[vname][:,ii]=tmp2d
-#
-#        vname = 'Mesh2_face_top_layer'
-#        #print '\tVariable: %s...'%vname
-#        ctop = sun.loadData(variable='ctop')
-#        #tmp2d = sun.Nkmax-ctop-1 # zero based
-#        tmp2d = sun.Nkmax-ctop # one based
-#        nc.variables[vname][:,ii]=tmp2d
-#
-#    print 72*'#'
-#    print '\t Finished SUNTANS->UnTRIM conversion'
-#    print 72*'#'
-#
-#
-#
-#
-#
-#
-#    # close the file
-#    nc.close()
+if __name__=='__main__':
+    # Command line use:
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mdu",help="MDU filename, e.g. 'my_run/flowfm.mdu'")
+    parser.add_argument("output",help="Output filename, e.g. 'hydro.nc'" )
+    parser.parse_args()
+    converter=DFlowToPTMHydro(parser.mdu,parser.output)
 
