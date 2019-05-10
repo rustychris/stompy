0372193acce8e59067b24fc8c3f6d2a9601e7d56
Rusty Holleman <rustychris@gmail.com>
2019-05-10 12:29:42 -0700

    Minor documentation for tom, and a sample script

--- test/test_tom.sh
+++ test/test_tom.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-python ../stompy/grid/tom.py -s data/scale-lines.shp -b data/dumbarton.shp 
+# Added -f 5.0 to coarsen the scale and complete much faster.
+# this should take about 1800 steps to complete, maybe 1 minute on a 2019 laptop.
+python ../stompy/grid/tom.py -f 5.0 -s data/scale-lines.shp -b data/dumbarton.shp 
 
 
 
