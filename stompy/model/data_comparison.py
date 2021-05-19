9d2263780c020bce5f2c6a91c25fc7bc38c27c01
Kenneth Larrieu <kglarrieu@ucdavis.edu>
2021-05-19 09:28:38 -0700

    extracting age tracer variables from model

--- stompy/model/data_comparison.py
+++ stompy/model/data_comparison.py
@@ -204,11 +204,10 @@ def assemble_comparison_data(models,observations,model_labels=None,
         # Not that many people use this...  but it's the correct one.
     elif base_var=='salinity':
         loc_extract_opts['data_vars']=['salinity']
+    elif base_var=='inorganic_nitrogen_(nitrate_and_nitrite)':
+        loc_extract_opts['data_vars']=['ZNit','NO3']  # want to extract both to calculate age and compare with nitrogen
     else:
-        try:
-            loc_extract_opts['data_vars']=[base_var]
-        except:
-            raise Exception("Not ready to extract variable %s"%base_var)
+        raise Exception("Not ready to extract variable %s"%base_var)
     
     loc_extract_opts.update(extract_options)
     
@@ -223,23 +222,24 @@ def assemble_comparison_data(models,observations,model_labels=None,
             print("No data extracted from model.  omitting")
             continue
             
-        assert len(loc_extract_opts['data_vars'])==1,"otherwise missing some data"
-        tgt_var=loc_extract_opts['data_vars'][0]
-        try:
-            da=ds[tgt_var]
-        except KeyError:
-            # see if the variable can be found based on standard-name
-            for dv in ds.data_vars:
-                if ds[dv].attrs.get('standard_name','')==tgt_var:
-                    da=ds[dv]
-                    da.name=tgt_var
-                    break
-            else:
-                raise Exception("Could not find %s by name or standard_name"%(tgt_var))
+        assert len(loc_extract_opts['data_vars'])>=1,"otherwise missing some data"
+        tgt_vars=loc_extract_opts['data_vars']
+        for tgt_var in tgt_vars:
+            try:
+                da=ds[tgt_var]
+            except KeyError:
+                # see if the variable can be found based on standard-name
+                for dv in ds.data_vars:
+                    if ds[dv].attrs.get('standard_name','')==tgt_var:
+                        da=ds[dv]
+                        da.name=tgt_var
+                        break
+                else:
+                    raise Exception("Could not find %s by name or standard_name"%(tgt_var))
 
-        da.name=base_var # having the same name helps later
-        da=da.assign_coords(label=label)
-        model_data.append(da)
+            da.name=base_var # having the same name helps later
+            da=da.assign_coords(label=label)
+            model_data.append(da)
         
     # Annotate the sources with labels
     for i,da in enumerate(observations):
