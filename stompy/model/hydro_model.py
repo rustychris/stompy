2aab878135c69791ba3a76a5e94382a8125ddf0a
Rusty Holleman <rustychris@gmail.com>
2021-07-08 09:20:13 -0500

    Avoid modifying BC dataArray during tzone shift

--- stompy/model/hydro_model.py
+++ stompy/model/hydro_model.py
@@ -365,7 +365,13 @@ class BC(object):
         return da
     def to_model_timezone(self,da):
         if 'time' in da.dims and self.model is not None:
-            da.time.values[:]=self.model.utc_to_native(da.time.values)
+            # da.time.values[:]=self.model.utc_to_native(da.time.values)
+            # Create new da and replace time data instead of mutating data.
+            # da.time may be shared by multiple instances. This way the
+            # call is idempotent and avoids nasty bugs
+            da_new=da.copy()
+            da_new['time']=('time',), self.model.utc_to_native(da.time.values)
+            return da_new
         return da
 
     def src_data(self):
