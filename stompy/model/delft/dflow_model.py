bc25616eb0ce5c9230f12793cbe9edae34a666ce
Rusty Chris Holleman <cdholleman@ucdavis.edu>
2022-03-20 15:06:49 -0700

    Better restart handling and sanitizing history ds

--- stompy/model/delft/dflow_model.py
+++ stompy/model/delft/dflow_model.py
@@ -425,17 +425,27 @@ class DFlowModel(hm.HydroModel,hm.MpiModel):
             self.run_dflowfm(cmd,mpi=False)
         else:
             # Copy the partitioned network files:
-            for proc in range(self.num_procs):
-                old_grid_fn=self.restart_from.subdomain_grid_filename(proc)
-                new_grid_fn=self.subdomain_grid_filename(proc)
-                print("Copying pre-partitioned grid files: %s => %s"%(old_grid_fn,new_grid_fn))
-                shutil.copyfile(old_grid_fn,new_grid_fn)
+            if self.restart_deep:
+                for proc in range(self.num_procs):
+                    old_grid_fn=self.restart_from.subdomain_grid_filename(proc)
+                    new_grid_fn=self.subdomain_grid_filename(proc)
+                    self.log.info("Copying pre-partitioned grid files: %s => %s"%(old_grid_fn,new_grid_fn))
+                    shutil.copyfile(old_grid_fn,new_grid_fn)
+            else:
+                self.log.info("Shallow restart, don't copy partitioned grid")
                 
             # not a cross platform solution!
             gen_parallel=os.path.join(self.dfm_bin_dir,"generate_parallel_mdu.sh")
             cmd=[gen_parallel,os.path.basename(self.mdu.filename),"%d"%self.num_procs,'6']
             return utils.call_with_path(cmd,self.run_dir)
 
+    @property
+    def restart_deep(self):
+        """
+        True if this is a restart and we are doing a deep copy
+        """
+        return bool(self.restart) and (self.run_dir!=self.restart_from.run_dir)
+        
     _dflowfm_exe=None
     @property
     def dflowfm_exe(self):
@@ -592,6 +602,9 @@ class DFlowModel(hm.HydroModel,hm.MpiModel):
             # the dwaq config depends on reading some of the DFM
             # details.
             self.dwaq.write_waq()
+
+        if self.restart_from is not None:
+            self.set_restart_file()
         
     def write_config(self):
         # Assumes update_config() already called
@@ -1101,20 +1114,24 @@ class DFlowModel(hm.HydroModel,hm.MpiModel):
         if set_coordinates:
             # Doctor up the dimensions
             # Misconfigured runs may have duplicates here.
-            cross_sections=[s.decode().strip() for s in his_ds.cross_section_name.values]
-            if len(cross_sections)>len(np.unique(cross_sections)):
-                print("Yuck - duplicate cross section names")
-                mask=np.zeros(len(cross_sections),np.bool8)
-                for i,crs in enumerate(cross_sections):
-                    mask[i]=crs not in cross_sections[:i]
-                    
-                his_ds=his_ds.isel(cross_section=mask)
-                cross_sections=np.array(cross_sections)[mask]
+
+            for coord,names in [ ('cross_section','cross_section_name'),
+                                 ('weigens','weigen_id'),
+                                 ('stations','station_name'),
+                                 ('general_structures','general_structure_id'),
+                                 ('gategens','gategen_name')]:
+                if names not in his_ds: continue
+                coord_vals=[s.decode().strip() for s in his_ds[names].values]
                 
-            his_ds['cross_section']=('cross_section',),cross_sections
-            
-            stations=[s.decode().strip() for s in his_ds.station_name.values]
-            his_ds['stations']=('stations',),stations
+                if len(coord_vals)>len(np.unique(coord_vals)):
+                    print('Yuck - duplicate %s names'%coord)
+                    mask=[val not in coord_vals[:i]
+                          for i,val in enumerate(coord_vals)]
+                    mask=np.array(mask, np.bool8 )
+                    his_ds=his_ds.isel(coord=mask)
+                    coord_vals==np.array(coord_vals)[mask]
+                    
+                his_ds[coord]=(coord,),coord_vals
 
         if decode_geometry:
             xr_utils.decode_geometry(his_ds,'cross_section_geom',replace=True)
@@ -1166,19 +1183,28 @@ class DFlowModel(hm.HydroModel,hm.MpiModel):
         self.mdu_basename=os.path.basename( model.mdu_basename.replace('.mdu',mdu_suffix+".mdu") )
         self.mdu.set_filename( os.path.join(self.run_dir, self.mdu_basename) )
         self.restart=True
-        self.restart_model=model
+        self.restart_from=model # used to be restart_model, but I don't think makes sense
         self.ref_date=model.ref_date
         self.run_start=model.restartable_time()
         assert self.run_start is not None,"Trying to restart run that has no restart data"
         
         self.num_procs=model.num_procs
         self.grid=model.grid
-        
+
+        # For now, these are synonymous
         if deep:
             assert self.run_dir != model.run_dir
-        
-        rst_base=os.path.join(model.mdu.output_dir(),
-                              (model.mdu.name
+        else:
+            assert self.run_dir == model.run_dir
+
+        self.set_restart_file()
+    def set_restart_file(self):
+        """ 
+        Update mdu['restart','RestartFile'] based on run_start and self.restart_from
+        Should be called when dealing with a restart and self.run_start is modified.
+        """
+        rst_base=os.path.join(self.restart_from.mdu.output_dir(),
+                              (self.restart_from.mdu.name
                                +'_'+utils.to_datetime(self.run_start).strftime('%Y%m%d_%H%M%S')
                                +'_rst.nc'))
         # That gets rst_base relative to the cwd, but we need it relative
