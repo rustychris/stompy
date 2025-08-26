Bug fixes but not yet robust in python depth-averaging code

stompy/model/delft/waq_scenario.py
@@ -1539,7 +1539,12 @@ class Hydro(object):
 
         link_and_signs=[] # (link idx, sign to make from->to same as left->right
         for a,b in zip(legs[:-1],legs[1:]):
-            link_and_sign = self.nodes_to_link_and_sign(a,b,on_edge)
+            link_and_sign = self.nodes_to_link_and_sign(a, b ,on_edge, on_boundary=on_boundary)
+            if link_and_sign is None:
+                # could be that a line cut across and island and with on_edge
+                # the edges are directly specified. Could also be that
+                continue
+            
             if link_and_signs and link_and_signs[-1][0]==link_and_sign[0]:
                 self.log.warning("Discarding repeated link")
             else:
@@ -1564,7 +1569,7 @@ class Hydro(object):
         g = self.grid()
         return g.cells_to_edge(c1,c2)
         
-    def nodes_to_link_and_sign(self,a,b,on_edge=False):
+    def nodes_to_link_and_sign(self, a, b, on_edge=False, on_boundary='warn_and_skip'):
         """
         a,b: node indices (0-based), assumed to be correct for self.grid()
         and flowgeom.
@@ -1577,14 +1582,10 @@ class Hydro(object):
         j=g.nodes_to_edge(a,b)
 
         if j is None:
-            # this happens when the line cuts across an island, and edges were
-            # specified directly. ignore the exchange.
             if on_edge:
-                continue
+                return None
             else:
-                # if legs came from shortest_path() above, it really shouldn't
-                # miss any edges, so signal bad news
-                raise Exception("edge couldn't be found, but it came from the grid.")
+                raise Exception("Failed to find edge")
 
         # possible to have missing cells with other marks (as in
         # marking an ocean or flow boundary), but boundary links are
@@ -1605,7 +1606,7 @@ class Hydro(object):
         if nhits==0:
             if np.any(c1_c2<0):
                 self.log.warning("Discarding boundary edge in path_to_transect_exchanges")
-                continue
+                return None
             else:
                 raise Exception("Failed to match edge to link")
         elif nhits>1:

stompy/model/openfoam/depth_average.py
@@ -91,7 +91,10 @@ class PostFoam:
     sim_dir = None
     verbose = False
     raster_mode = 'clipping' # or 'bbox'
-    
+
+    # mesh_slice_method=='numba'
+    mesh_slice_method='python'
+
     max_n_procs=1024 # just to bound the search, no inf loops
     precision=15
     current_timename = None
@@ -458,7 +461,7 @@ class PostFoam:
                 return pickle.load(fp)
         
         mesh_state = self.read_mesh_state(proc)
-        raster_weights = precalc_raster_weights_proc_by_faces(fld, *mesh_state)
+        raster_weights = precalc_raster_weights_proc_by_faces(fld, *mesh_state, mesh_slice_method=self.mesh_slice_method)
         if cache_fn is not None:
             if not os.path.exists(cache_dir):
                 os.makedirs(cache_dir)
@@ -938,7 +941,7 @@ def clipped_volume(edges, xmin=None, xmax=None, ymin=None, ymax=None,
 #             pickle.dump(raster_weights,fp,-1)
 #     return raster_weights
 
-def precalc_raster_weights_proc_by_faces(fld, xyz, face_nodes, face_cells, cell_faces):
+def precalc_raster_weights_proc_by_faces(fld, xyz, face_nodes, face_cells, cell_faces, mesh_slice_method='numba'):
     """
     This version should be much faster. It avoids qhull, slices faces rather than clipping
     cells, and uses a padded array mesh representation that's faster for both python and
@@ -957,7 +960,10 @@ def precalc_raster_weights_proc_by_faces(fld, xyz, face_nodes, face_cells, cell_
     fld_x,fld_y = fld.xy() # I think these are pixel centers
 
     # unclear whether numba version is sketch or not.
-    slicer = mesh_ops.mesh_slice_nb # mesh_slice_nb
+    if mesh_slice_method=='numba':
+        slicer = mesh_ops.mesh_slice_nb # mesh_slice_nb
+    else:
+        slicer = mesh_ops.mesh_slice # mesh_slice_nb
 
     if 0: # debugging checks
         print("Checking cells before any operations")
@@ -968,6 +974,7 @@ def precalc_raster_weights_proc_by_faces(fld, xyz, face_nodes, face_cells, cell_
                 mesh_ops.mesh_check_cell(cIdx,True,mesh_state)
                 
         print("Check complete")
+    assert np.all(mesh_state[1][:,:3]>=0)
 
     cell_mapping=None
     for col in utils.progress(range(fld.shape[1])):
@@ -975,6 +982,7 @@ def precalc_raster_weights_proc_by_faces(fld, xyz, face_nodes, face_cells, cell_
         xmin=fld_x[col]-fld.dx/2
         xmax=fld_x[col]+fld.dx/2
         cell_mapping, mesh_state = slicer(np.r_[1,0,0], xmin, cell_mapping, *mesh_state)
+        assert np.all(mesh_state[1][:,:3]>=0) # col 232 is leaving this corrupt.
 
     if 0: # debugging checks
         print("Checking cells")
@@ -983,8 +991,9 @@ def precalc_raster_weights_proc_by_faces(fld, xyz, face_nodes, face_cells, cell_
                 print(f"cIdx={cIdx} failed check")
                 mesh_ops.mesh_check_cell(cIdx,True,mesh_state)
                 raise Exception(f"cIdx={cIdx} failed check")
-                
         print("Check complete")
+    if 1: # and check that all faces have at least 3 nodes
+        assert np.all(mesh_state[1][:,:3]>=0)
     
     # Slice the mesh so all cells are in exactly one pixel
     print("Slicing by row")
@@ -993,6 +1002,10 @@ def precalc_raster_weights_proc_by_faces(fld, xyz, face_nodes, face_cells, cell_
         if row==0: continue
         ymin=fld_y[row]-fld.dy/2
         ymax=fld_y[row]+fld.dy/2
+        # if row==5: 
+        #     import pdb
+        #     print("DEBUG: about to hit 2-node face bug")
+        #     pdb.set_trace()
         cell_mapping, mesh_state = slicer(np.r_[0,1,0], ymin, cell_mapping, *mesh_state)
 
     if 0: # debugging checks
@@ -1000,6 +1013,8 @@ def precalc_raster_weights_proc_by_faces(fld, xyz, face_nodes, face_cells, cell_
         for cIdx in utils.progress(range(len(mesh_state[3])), func=print):
             assert mesh_ops.mesh_check_cell(cIdx,False,mesh_state)
         print("Check complete")
+    if 1: # and check that all faces have at least 3 nodes
+        assert np.all(mesh_state[1][:,:3]>=0)
 
     print("Calculate volume")
     volumes,centers = mesh_ops.mesh_cell_volume_centers_nb(*mesh_state)

stompy/model/openfoam/mesh_ops.py
@@ -1,13 +1,15 @@
 import os
 import numpy as np
+from ... import utils
 from numba import njit
 from numba.typed import List
+    
 import time
 from fluidfoam.readof import OpenFoamFile
-from ... import utils
 
-FACE_MAX_NODES=20
-CELL_MAX_FACES=20
+# Did run into an issue with MR long v3 that hit the cell-faces limit of 20
+FACE_MAX_NODES=40
+CELL_MAX_FACES=40
 NO_FACE=np.iinfo(np.int32).max
 
 
@@ -59,6 +61,15 @@ def load_mesh_state(case_dir,precision=15):
 
     print(f"Time to convert mesh representation: {time.time()-t:.3f}s")
     return (xyz,face_nodes,face_cells,cell_faces)
+
+def mesh_bbox(case_dir,precision=15):
+    # Load proc mesh                    
+    verbose=False
+    meshpath = os.path.join(case_dir,'constant/polyMesh')
+    pointfile = OpenFoamFile(meshpath, name="points", precision=precision, verbose=verbose)
+
+    xyz=pointfile.values.reshape([-1,3])
+    return np.array([xyz.min(axis=0),xyz.max(axis=0)])
 
 def mesh_rotate(rot,xyz,face_nodes,face_cells,cell_faces):
     xyz=np.tensordot(xyz,rot,[-1,0])
@@ -308,7 +319,7 @@ def mesh_check_cell(cIdx,verbose,mesh_state):
     # these come out with wrong sign.
     if verbose:
         print("Outward components: "," ".join([f"{comp:.2e}" for comp in outwards])) # expect all positive
-        if np.all(comp==0):
+        if np.all(outwards==0):
             import pdb
             pdb.set_trace()
 
@@ -369,7 +380,7 @@ def mesh_slice(slice_normal, slice_offset, cell_mapping, xyz, face_nodes, face_c
     # size_xyz=[-1,1,-1,1]. Forcing those nodes to be on the slice plane leads to issues
     # with adjacent faces. Instead, triangulate.
     if 1:
-        n_face_orig = faces_to_slice.shape[0]
+        n_face_orig = face_nodes.shape[0] # had been wrongly faces_to_slice
         
         for fIdx in faces_to_slice:
             nodes = face_nodes[fIdx]
@@ -415,12 +426,13 @@ def mesh_slice(slice_normal, slice_offset, cell_mapping, xyz, face_nodes, face_c
                 print(f"Triangulating warped face on slice plane for fIdx={fIdx}")
                 (xyz, face_nodes, face_cells, cell_faces) = mesh_triangulate(fIdx, 
                                                                              xyz, face_nodes, face_cells, cell_faces)
+                assert np.all(face_nodes[:,:3]>=0)
 
         tri_faces = np.arange(n_face_orig,face_nodes.shape[0])
         if len(tri_faces):
             faces_to_slice = np.concatenate((faces_to_slice,tri_faces))
         
-    #print(f"At offset {slice_offset} will slice {len(faces_to_slice)} faces")
+    print(f"At offset {slice_offset} will slice {len(faces_to_slice)} faces")
 
     # slices actually have to be per edge, not face.
     sliced_edges={} # (edge small node, edge large node) => new node
@@ -431,6 +443,8 @@ def mesh_slice(slice_normal, slice_offset, cell_mapping, xyz, face_nodes, face_c
     n_face_orig = face_nodes.shape[0]
     n_node_orig = xyz.shape[0]
 
+    assert np.all(face_nodes[:,:3]>=0)
+
     for fIdx in faces_to_slice:
         nodes = face_nodes[fIdx]
         for node_i in range(FACE_MAX_NODES):
@@ -440,6 +454,7 @@ def mesh_slice(slice_normal, slice_offset, cell_mapping, xyz, face_nodes, face_c
         else:
             node_count=FACE_MAX_NODES
         nodes = nodes[:node_count]
+        assert len(nodes)>=3 # having some corruption issues...
 
         # use side_xyz for fine-grained control
         #cmp_nodes = cmp_xyz[nodes[:node_count]]
@@ -596,6 +611,14 @@ def mesh_slice(slice_normal, slice_offset, cell_mapping, xyz, face_nodes, face_c
         assert count_neg>=3 
         assert count_pos>=3
 
+        # # DBG - make sure we're not creating a degenerate triangle
+        # for new_faces in [nodes_neg,nodes_pos]:
+        #     tmp_nodes = new_faces[new_faces>=0]
+        #     tmp_nodes.sort()
+        #     assert np.all(tmp_nodes[1:] - tmp_nodes[:-1])>0
+        #     assert(len(tmp_nodes)>=3)
+        # # /DBG
+                
         face_nodes[fIdx]=nodes_neg
         newFIdx = len(face_nodes)
         face_nodes = array_append(face_nodes, nodes_pos)
@@ -610,6 +633,9 @@ def mesh_slice(slice_normal, slice_offset, cell_mapping, xyz, face_nodes, face_c
         cIdx = face_cells[fIdx,0] # owner
         assert cIdx>=0
         sliced_cells[cIdx] = -1 # sliced, but no new cell has been created
+        # this is failing with cell_n_faces[cIdx]==20, but dimensions is 20.
+        if cell_n_faces[cIdx]==CELL_MAX_FACES:
+            raise Exception(f"cell_n_faces[cIdx=={cIdx}]=={CELL_MAX_FACES} - cannot add another face")
         cell_faces[cIdx,cell_n_faces[cIdx]]=newFIdx
         cell_n_faces[cIdx]+=1
 
@@ -625,7 +651,15 @@ def mesh_slice(slice_normal, slice_offset, cell_mapping, xyz, face_nodes, face_c
     cells_to_sort = list(sliced_cells.keys())
     cells_to_sort.sort()
 
+    assert np.all(face_nodes[:,:3]>=0)
+
     for cIdx in cells_to_sort:
+        # # DBG
+        # if cIdx==91200 and len(cells_to_sort)==1265:
+        #     print("Walk through and check for a sketchy pair of edges including 217145,217146")
+        #     import pdb
+        #     pdb.set_trace()
+        # # /DBG
         faces = cell_faces[cIdx,:cell_n_faces[cIdx]]
         cell_face_neg = np.full(CELL_MAX_FACES,NO_FACE,np.int32)
         count_neg=0
@@ -728,6 +762,7 @@ def mesh_slice(slice_normal, slice_offset, cell_mapping, xyz, face_nodes, face_c
                     break
             assert new_face_node[nfn_count-1]==new_face_node[0]
             new_face_node[nfn_count-1]=-1
+            assert (new_face_node>=0).sum() >= 3
 
             
         new_fIdx = face_nodes.shape[0]
@@ -783,6 +818,9 @@ def mesh_slice(slice_normal, slice_offset, cell_mapping, xyz, face_nodes, face_c
                 else:
                     assert False,"Failed to find face to flip"
 
+    assert np.all(face_nodes[:,:3]>=0)
+                
+                    
     return cell_mapping, (xyz, face_nodes, face_cells, cell_faces)
 
 
@@ -1345,6 +1383,9 @@ def mesh_triangulate(fIdx,
         tri_nodes[1] = nodes[i_tri+1]
         tri_nodes[2] = nodes[i_tri+2]
 
+        # DBG: tracking down degenerates
+        assert tri_nodes[0]!=tri_nodes[1] and tri_nodes[1]!=tri_nodes[2] and tri_nodes[2]!=tri_nodes[0]
+
         if i_tri==0:
             face_nodes[fIdx] = tri_nodes
         else:
@@ -1369,6 +1410,8 @@ def mesh_triangulate_nb(fIdx,
     nodes = face_nodes[fIdx]
     nodes = nodes[nodes>=0] # this will be a copy, so it's safe to mutate face_nodes below
 
+    assert len(nodes)>=3
+    
     owner,nbr = face_cells[fIdx]
     n_face_owner = (cell_faces[owner]!=NO_FACE).sum()
     if nbr>=0: